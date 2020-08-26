2563173b04bb659bb737f5474d13760d524fba2b
Chege Bryan <cbbryan8@gmail.com>
2020-08-26 20:24:46 +0300

    Add tests setup method

--- length/tests.py
+++ length/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestLengthConversion(TestCase):
+    """
+    This class contains tests that convert measurements from one unit
+    of measurement to another
+    """
+
+    def setUp(self):
+        """
+        This method runs before the execution of each test case
+        """
+        self.client = Client()
+        self.url = reverse('length:convert')
