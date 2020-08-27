accc3ac2098b45cf53efb32624a009f20f724e7a
Chege Bryan <cbbryan8@gmail.com>
2020-08-27 14:20:38 +0300

    Add length converter view

--- length/views.py
+++ length/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+from length.forms import LengthConverterForm
+
+convert_to_metre = {
+    "centimetre": 0.01,
+    "metre": 1.0
+}
+
+convert_from_metre = {
+    "centimetre": 100,
+    "metre": 1.0
+}
+
+
+def convert(request):
+    form = LengthConverterForm()
+    if request.GET:
+        input_unit = request.GET['input_unit']
+        input_value = request.GET['input_value']
+        output_unit = request.GET['output_unit']
+
+        metres = convert_to_metre[input_unit] * float(input_value)
+        output_value = metres * convert_from_metre[output_unit]
+
+        data = {
+            'input_unit': input_unit,
+            'input_value': input_value,
+            'output_unit': output_unit,
+            'output_value': round(output_value, 3)
+        }
+
+        form = LengthConverterForm(initial=data)
+        return render(
+            request, 'length.html', context={'form': form}
+        )
+    return render(request, 'length.html', context={'form': form})
